8bf6fea9a0dd315853a504a53b62b82ebace205b
prajeeshp007 <prajeeshprmod1@gmail.com>
2024-04-23 13:18:47 +0530

    commit

---
+++ bin/flutter.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/flutter.dart' as flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter.calculate()}!');
+}
---
+++ bin/prajeeshfirst_programme.dart
@@ -0,0 +1,19 @@
+void main(){
+  ///variable creation
+  /// :- datatype variablename = value;
+  String name = "prajeesh";
+  int age = 22;
+  double cgpa = 7;
+  String email ="prajeesh pramod 1@gmail.com";
+  int phonenumber = 9074230961;
+  var course ="B.com";
+  print("my name is $name");
+  print("iam $age yrs old");
+  print("my gmail is $email");
+  print("my phonenumber is $phonenumber");
+  print("i am a $course graduate");
+  int a = 10, b = 20;
+  print('sum = ${a+b} ');
+
+}
+
