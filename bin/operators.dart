495d96ffbe01665869cb253d2aaf0103116ea5bc
prajeeshp007 <prajeeshprmod1@gmail.com>
2024-04-25 13:03:31 +0530

    commit

---
+++ bin/operators.dart
@@ -0,0 +1,16 @@
+void main(){
+  ///relation operator
+  num a = 9, b = 4;
+
+  print('$a>$b = ${a>b}');
+  print('$a<$b = ${a<b}');
+  print("$a>=$b=${a<=b}");
+  print('$a>=$b =${a>=b}');
+  print('$a==$b =${a==b}');
+  print('a!=$b = ${a!=b}');
+
+
+
+
+
+}
